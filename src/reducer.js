a2eea8a39a14995db76663975e3a00cfcf6dffb9
unmyke <unmyke@gmail.com>
2020-02-22 05:49:35 +0800

    fix(reducer): protect against a negative state value
    State represents stopwatch time interval, so it must not to be negative.

--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ import { CHANGE_INTERVAL } from "./actions";
 const reducer = (state = 0, action) => {
   switch (action.type) {
     case CHANGE_INTERVAL:
-      return (state += action.payload);
+      return state <= -action.payload ? 0 : state + action.payload;
     default:
       return state;
   }
