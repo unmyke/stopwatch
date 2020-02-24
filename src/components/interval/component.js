2a74a599850a93023c1a6854828c5254b885c7bc
unmyke <unmyke@gmail.com>
2020-02-24 23:56:05 +0800

    feat(components): protect against negative interval setup

--- src/components/interval/component.js
+++ src/components/interval/component.js
@@ -2,16 +2,23 @@ import React from "react";
 
 class IntervalComponent extends React.Component {
   render() {
+    const { currentInterval, changeInterval } = this.props;
+    const decrementIsDisabled = currentInterval === 0;
+
     return (
       <div>
         <span>
           Интервал обновления секундомера: {this.props.currentInterval} сек.
         </span>
         <span>
-          <button type="button" onClick={() => this.props.changeInterval(-1)}>
+          <button
+            type="button"
+            onClick={() => changeInterval(-1)}
+            disabled={decrementIsDisabled}
+          >
             -
           </button>
-          <button type="button" onClick={() => this.props.changeInterval(1)}>
+          <button type="button" onClick={() => changeInterval(1)}>
             +
           </button>
         </span>
